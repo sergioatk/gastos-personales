cefa20a80438a0168b035111171ea71c7a2d6feb
Sergio Tkaczek <sergioatk@gmail.com>
2021-09-22 13:04:31 -0300

    deja configurado handlebars

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
 const express = require('express');
-const exphdb = require('express-handlebars');
+const exphbs = require('express-handlebars');
 const path = require('path');
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.get('/', (req, res) => res.send('hola'))
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.engine(
+    "handlebars",
+    exphbs({
+        defaultLayout: "main",
+        runtimeOptions: {
+            allowProtoPropertiesByDefault: true,
+            allowProtoMethodsByDefault: true,
+        },
+    })
+);
+app.set("view engine", "handlebars");
+
+app.get('/', (req, res) => {
+    res.render('view', {
+        layout: 'main',
+        data: {
+            titulo: 'Lista de Clubes de Inglaterra',
+            subTitulo: 'Lista actual'
+        }
+    })
+})
 
 app.listen(PORT, console.log(`escuchando en el puerto ${PORT}`))
